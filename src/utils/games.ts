456e7002268cc298a80d8b0e6e433990f379fa38
Themanuca <ssergioca@gmail.com>
2022-09-22 18:53:43 -0300

    Adicionando assets, theme e terminando o ultils

---
+++ src/utils/games.ts
@@ -0,0 +1,14 @@
+export const GAMES = [
+  {
+    id: "1",
+    name: "League of Legends",
+    ads: "4",
+    cover: require("../assets/games/game1.png"),
+  },
+  {
+    id: "2",
+    name: "Dota 2",
+    ads: "3",
+    cover: require("../assets/games/game2.png"),
+  },
+];
