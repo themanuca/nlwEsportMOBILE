31c4d60f7e22a35dd2a45f9143df057e29c1c531
Themanuca <ssergioca@gmail.com>
2022-10-02 22:30:45 -0300

    feat: Lista de games, pegada carrossel

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, FlatList } from "react-native";
 import logoImg from "../../../assets/icon.png";
+import { GameCard } from "../../Components/GameCard";
 import { Heading } from "../../Components/Heading";
 import { styles } from "./styles";
-
+import { GAMES } from "../../utils/games";
 export function Home() {
   return (
     <View style={styles.container}>
@@ -11,6 +11,14 @@ export function Home() {
       <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar"
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
